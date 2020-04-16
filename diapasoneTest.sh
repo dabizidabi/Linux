83b2338cf7978c1d5418b33907f4a24efdca5a4c
Mykola Zoshchuk <60040394+dabizidabi@users.noreply.github.com>
2020-04-16 21:55:31 +0300

    add new file

---
+++ diapasoneTest.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+INT=$1
+MAX=50
+MIN=1
+
+if [[ $INT =~ ^-?[0-9]+$ ]]; then
+        if [[ $INT -le $MAX && $INT -ge $MIN ]]; then
+                echo "$INT is in diapasone 0-50"
+        elif [[ $INT = 0 ]]; then
+                echo "INT is zero"
+        elif [[ $INT =~ ^-?0+$ ]]; then
+                echo "INT zeerrrrooo"
+        else
+                echo "INT isnt in diapasone"
+        fi
+else
+        echo "INT is no integer"
+fi
