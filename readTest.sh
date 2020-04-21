37026c5c009f02a86c10ba21cf5e1e4f295b2d5f
Mykola Zoshchuk <dabizidabi@gmail.com>
2020-04-21 20:38:57 +0300

    add read

---
+++ readTest.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo -n "Write values "
+
+read var1 var2 var3 var4
+
+echo "Value1 = $var1"
+echo "Value2 = $var2" 
+echo "Value3 = $var3"
+echo "Value4 = $var4"
+echo "..." 
+
+echo "Value1 = $1"
+echo "Value2 = $2"
+echo "..." 
