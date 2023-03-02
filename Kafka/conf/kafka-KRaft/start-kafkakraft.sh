4bf8ace83a3c0afc75ae828abea99f87589a339e
Mykola Zoshchuk <dabizidabi@gmail.com>
2023-03-02 22:54:12 +0200

    [feat] rewrite kafka-kraft for using bitnami image

--- Kafka/conf/kafka-KRaft/start-kafkakraft.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -e
-
-KAFKA_CLUSTER_ID="$(bin/kafka-storage.sh random-uuid)" && \
-"/kafka/bin/kafka-storage.sh" format -t $KAFKA_CLUSTER_ID -c "/kafka/config/kraft/server.properties" && \
-"/kafka/bin/kafka-server-start.sh" "/kafka/config/kraft/server.properties"
